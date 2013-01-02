df08cf00f4c68ebdf641e7a90fc7a871f73f14e2
Evgeny Demchenko <slam@sterno.ru>
2013-01-02 16:58:39 +0800

    Initial empty app and packaging tools

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# This is my README
+# GoScale CMS
+
+Detailed documentation is in the "docs" directory.
+
+Quick start
+-----------
+
+2. Add "goscale" to your INSTALLED_APPS setting like this::
+
+      INSTALLED_APPS = (
+          ...
+          'goscale',
+      )
---
+++ setup.py
@@ -0,0 +1,22 @@
+import ez_setup
+import os
+ez_setup.use_setuptools()
+from setuptools import setup, find_packages
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+setup(
+    name = "goscalecms",
+    version = "0.1",
+    packages = find_packages(),
+    author = "Evgeny Demchenko",
+    author_email = "little_pea@list.ru",
+    description = "GoScale CMS",
+    long_description = README,
+    url = "https://bitbucket.org/littlepea12/goscale/",
+    include_package_data = True,
+    install_requires = [
+        "Django >= 1.1.1",
+        "django-cms >= 2.2",
+    ]
+)
