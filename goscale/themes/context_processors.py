90d3cd6c2c8e5a530ada28f11aa8a504d0fa3682
littlepea <little_pea@list.ru>
2013-04-21 15:42:24 +0800

    goscale/themes/context_processors.py

--- goscale/themes/context_processors.py
+++ goscale/themes/context_processors.py
@@ -9,7 +9,8 @@ def theme(request):
     # set  config
     site_theme = settings.THEME
     static_theme_url = '%sthemes/%s/static/' % (settings.STATIC_URL, site_theme)
-    config =  {}
+    static_common_url = '%sthemes/common/static/' % settings.STATIC_URL
+    config = {}
     settings_list = ['GOSCALE_BOOTSTRAP_THEMES', 'GOSCALE_BOOTSTRAP_THEME',
                      'GOSCALE_AJAXLINKS', 'GOSCALE_AJAXLINKS_EXCEPTIONS']
     for setting in settings_list:
@@ -20,6 +21,7 @@ def theme(request):
         'site': get_current_site(request),
         'GOSCALE_THEME': site_theme,
         'STATIC_THEME_URL': static_theme_url,
+        'STATIC_COMMON_URL': static_common_url,
         'goscale_config': config,
         'goscale_bootstrap_theme': theme if theme else conf.GOSCALE_BOOTSTRAP_THEME,
         'goscale_config_json': simplejson.dumps(config),
